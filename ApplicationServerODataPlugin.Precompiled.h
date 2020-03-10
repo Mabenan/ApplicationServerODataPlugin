b678a2eb47f608116ae1bd121fee93b707cde72a
Mabenan <doener123@googlemail.com>
2020-03-10 15:51:44 +0100

    removed cxx

---
+++ ApplicationServerODataPlugin.Precompiled.h
@@ -0,0 +1,16 @@
+/*
+ * precompiled.h
+ *
+ *  Created on: 29.02.2020
+ *      Author: doene
+ */
+
+#ifndef APPLICATION_SERVER_ODATA_PLUGIN_PRECOMPILED_H_
+#define APPLICATION_SERVER_ODATA_PLUGIN_PRECOMPILED_H_
+
+#include <QxOrm.h>
+
+#include <ApplicationServerODataPlugin_global.h>
+#include <OData>
+
+#endif /* REPS_HERP_AUTHORIZATIONMODULE_HERP_AUTHORIZATIONMODULE_PRECOMPILED_H_ */
